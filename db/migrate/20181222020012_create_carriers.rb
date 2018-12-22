f9974383dfc4b1aca3376d91334fa8c2d7bab801
william bowa <william.bowa@mediapro.com>
2018-12-21 18:02:17 -0800

    scaffold carrier model

---
+++ db/migrate/20181222020012_create_carriers.rb
@@ -0,0 +1,11 @@
+class CreateCarriers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :carriers do |t|
+      t.string :name
+      t.string :description
+      t.integer :extension
+
+      t.timestamps
+    end
+  end
+end

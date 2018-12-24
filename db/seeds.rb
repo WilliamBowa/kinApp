bb252492ee8f9370069538931161f567a6ca0667
william bowa <william.bowa@mediapro.com>
2018-12-23 22:31:36 -0800

    Add seed & carrier default root route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seeding the 4 Phone Carriers
+
+Carrier.create(name: "Vodacom", description: "This is Vodacom", extension: 81)
+Carrier.create(name: "Airtel",  description: "This is Airtel", extension: 99)
+Carrier.create(name: "Orange",  description: "This is Orange", extension: 81)
+Carrier.create(name: "Africel", description: "This is Africel", extension: 86)

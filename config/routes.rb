3fd6258c042b689cf5c2d71fb8c69925a2f3bef7
william bowa <william.bowa@mediapro.com>
2019-02-04 21:51:56 -0800

    customize users route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :carriers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users, :path_prefix => 'd'
+  # devise_for :users, :path_prefix => 'd'
+  devise_for :users, :path_prefix => '', path: '', 
+              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
+  # route for user controler overriding devise            
   resources :users
 
   root to: 'carriers#index'
